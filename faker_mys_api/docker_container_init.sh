7cffc833fba0ca32bbcf1fec8d2c54983acd465f
mabmdevx <mea.devx@gmail.com>
2023-10-08 15:51:54 -0700

    Faker Mock YAML Server - Fixes 12

--- faker_mys_api/docker_container_init.sh
+++ faker_mys_api/docker_container_init.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Params
-node_module_global_path="/usr/local/lib/node_modules"
-
 # Start Node Application
 echo -e "\nStarting the Mock YAML Server ...\n"
 npm install -g mock-yaml-server
-mkdir /"$node_module_global_path/mock-yaml-server/resources"
-cp users.yaml /"$node_module_global_path/mock-yaml-server/resources"
-mock-yaml-server &
+mock-yaml-server -f /usr/src/resources &
 echo -e "\nDone"
 
 # Start Node Application
